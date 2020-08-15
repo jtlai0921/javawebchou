4fc8515ec1de683ccd919b653495ae6782d2b32d
賴紀廷 <you@example.com>
2020-08-15 10:17:34 +0800

    1
    1

---
+++ eojp501it6/JavaWebSample/JavaWebSample/mod04/src/com/gjun/controller/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.gjun.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ eojp501it6/JavaWebSample/JavaWebSample/mod04/src/com/gjun/controller/HelloControllerTestTest.java
@@ -0,0 +1,14 @@
+package com.gjun.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
